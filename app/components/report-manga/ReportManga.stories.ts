1a05fb3be0ee0d653a1d3caab67bcf4e8137b597
tdquang266 <quangjackson995@gmail.com>
2023-08-08 21:32:44 +0700

    add 4 components

---
+++ app/components/report-manga/ReportManga.stories.ts
@@ -0,0 +1,16 @@
+import {Meta, StoryObj} from "@storybook/react";
+import ReportManga from "./ReportManga";
+
+const meta: Meta<typeof ReportManga> = {
+    title: 'Components/ReportManga',
+    component: ReportManga,
+    argTypes: {
+        onSend: { action: 'onSend' },
+        onClose: { action: 'onClose' }
+    },
+};
+
+export default meta;
+type Story = StoryObj<typeof ReportManga>;
+
+export const Default: Story = {};
---
+++ app/components/report-manga/ReportManga.tsx
@@ -0,0 +1,60 @@
+import React, { useState } from "react";
+import "../../globals.css";
+import "./ReportManga.css";
+
+type ReportMangaProps = {
+    onSend: () => void;
+    onClose: () => void;
+};
+
+const ReportManga: React.FC<ReportMangaProps> = ({ onSend, onClose }) => {
+    const [isSendClicking, setIsSendClicking] = useState(false);
+    const [isCloseClicking, setIsCloseClicking] = useState(false);
+
+    const handleSendClick = () => {
+        onSend();
+        setIsSendClicking(true);
+    };
+
+    const handleCloseClick = () => {
+        onClose();
+        setIsCloseClicking(true);
+    };
+
+    return (
+        <div className="font-primary w-[320px] h-fit flex flex-col gap-[30px] p-5 bg-richBlack/[.55] backdrop-blur-[10px] rounded-3xl border-[1.5px] border-white/20">
+            <div className="w-full h-fit flex flex-col gap-2.5">
+                <p className="text-sm text-lightGray font-semibold">Báo lỗi truyện</p>
+                <p className="text-xs text-white opacity-75 font-medium">Cảm ơn bạn đã hỗ trợ báo lỗi, truyện sẽ được fix trong thời gian sớm nhất.</p>
+            </div>
+            <div className="w-full h-fit flex flex-col gap-5">
+                <label className="w-full h-fit flex flex-row gap-2.5">
+                    <input type="checkbox" name="option" className="w-[16px] h-[16px]"/>
+                    <span className="text-xs text-lightGray font-medium">Ảnh bị lỗi, không thấy ảnh</span>
+                </label>
+                <label className="w-full h-fit flex flex-row gap-2.5">
+                    <input type="checkbox" name="option" className="w-[16px] h-[16px]"/>
+                    <span className="text-xs text-lightGray font-medium">Chapter bị trùng</span>
+                </label>
+                <label className="w-full h-fit flex flex-row gap-2.5">
+                    <input type="checkbox" name="option" className="w-[16px] h-[16px]"/>
+                    <span className="text-xs text-lightGray font-medium">Chapter chưa dịch</span>
+                </label>
+                <label className="w-full h-fit flex flex-row gap-2.5">
+                    <input type="checkbox" name="option" className="w-[16px] h-[16px]"/>
+                    <span className="text-xs text-lightGray font-medium">Up sai truyện</span>
+                </label>
+            </div>
+            <div className="w-full h-fit flex flex-row justify-end items-center gap-[15px]">
+                <div className={`flex px-[14px] py-[10px] rounded-full bg-success cursor-pointer ${isSendClicking ? 'active' : ''}`} onClick={handleSendClick}>
+                    <span className="text-xs text-white font-semibold">Gửi đi</span>
+                </div>
+                <div className={`flex px-[14px] py-[10px] rounded-full bg-lightGray cursor-pointer ${isCloseClicking ? 'active' : ''}`} onClick={handleCloseClick}>
+                    <span className="text-xs text-black font-semibold">Đóng</span>
+                </div>
+            </div>
+        </div>
+    );
+};
+
+export default ReportManga;
