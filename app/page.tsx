4a69fbf2e1a5dbb42024e8486f7424fd9e2426da
tdquang <quangjackson995@gmail.com>
2023-09-29 11:02:05 +0700

    Add MangaRead component

--- app/page.tsx
+++ app/page.tsx
@@ -13,23 +13,25 @@ export default function Home() {
       <div className="w-screen min-h-screen flex justify-center items-center bg-richBlack">
         <div className="hidden w-[1200px] h-full tablet:flex flex-col justify-start items-center gap-[50px] px-[40px]">
           <NavBar isHomePage={true} />
-          <SourceBar />
-          <Suspense fallback={<Loading />}>
-            <NewMangaUpdate />
-          </Suspense>
-          <div className="w-full h-[50px]"></div>
-          <div className="w-full h-fit flex flex-row justify-between items-center pt-[25px] py-[50px] border-t-[1.5px] border-white/[.15]">
-            <div className="flex flex-col gap-[6px]">
-              <p className="text-lg text-lightGray font-semibold uppercase tracking-wider">
-                AnimeMoi
-              </p>
+          <div className="w-full min-h-[calc(100vh-90px-50px)] flex flex-col gap-[50px]">
+            <SourceBar />
+            <Suspense fallback={<Loading />}>
+              <NewMangaUpdate />
+            </Suspense>
+            <div className="w-full h-[50px]"></div>
+            <div className="w-full h-fit flex flex-row justify-between items-center pt-[25px] py-[50px] border-t-[1.5px] border-white/[.15]">
+              <div className="flex flex-col gap-[6px]">
+                <p className="text-lg text-lightGray font-semibold uppercase tracking-wider">
+                  AnimeMoi
+                </p>
+                <p className="text-sm text-white/75 font-medium">
+                  1 sản phẩm của tdquang266 và hoang3402
+                </p>
+              </div>
               <p className="text-sm text-white/75 font-medium">
-                1 sản phẩm của tdquang266 và hoang3402
+                © 2023 AnimeMoi. Tất cả quyền được bảo lưu.
               </p>
             </div>
-            <p className="text-sm text-white/75 font-medium">
-              © 2023 AnimeMoi. Tất cả quyền được bảo lưu.
-            </p>
           </div>
         </div>
         <div className="tablet:hidden w-full h-full flex flex-col justify-center items-center gap-[50px]">
@@ -56,12 +58,3 @@ export default function Home() {
     </SourceProvider>
   );
 }
-
-/*
-1. Move checkAuth function from NavBar to Firebase
-2. Add a command to handle logic for the search bar (still haven't assigned the event to jump to the details page when clicking on any manga in the SearchResult component)
-3. Change fetching Genre data from URL to fetch from local JSON file
-4. Add text to manga status instead of displaying numbers
-5. Edit the image displayed on the notification page that does not support responsive
-6. Optimized the command line in some files
-*/
--- app/pages/reader/[host]/page.tsx
+++ app/pages/reader/[host]/page.tsx
@@ -1,29 +1,50 @@
 import Image from "next/image";
-import Link from "next/link";
-import { Domain, DomainGetImage } from "../../../domain";
+import Thumbnail from "../../../images/thumbnail.png";
+import NavBar from "../../../components/nav-bar/NavBar";
+import MangaRead from "../../../components/manga-read/MangaRead";
 
 export default async function Page(params: any) {
-  const response = await fetch(
-    `${Domain}${params.params.host}/ChapterDetail?url=${params.searchParams.id}`
-  );
-  const data = await response.json();
-
   return (
-    <>
-      <button>
-        <Link href={`/`}>Back</Link>
-      </button>
-      <div className={"flex flex-col items-center"}>
-        {data.map((chapter: any) => (
-          <Image
-            key={chapter}
-            src={`${DomainGetImage}${params.params.host}/GetImage?url=${chapter}`}
-            alt={""}
-            width={"600"}
-            height={"800"}
-          />
-        ))}
+    <div className="w-screen min-h-screen flex justify-center items-center bg-richBlack">
+      <div className="hidden w-[1200px] h-full tablet:flex flex-col justify-start items-center gap-[50px] px-[40px]">
+        <NavBar isHomePage={false} />
+        <div className="w-full min-h-[calc(100vh-90px-50px)] flex flex-col gap-[50px]">
+          <MangaRead host={params.params.host} params={params} />
+          <div className="w-full h-[50px]"></div>
+          <div className="w-full h-fit flex flex-row justify-between items-center pt-[25px] py-[50px] border-t-[1.5px] border-white/[.15]">
+            <div className="flex flex-col gap-[6px]">
+              <p className="text-lg text-lightGray font-semibold uppercase tracking-wider">
+                AnimeMoi
+              </p>
+              <p className="text-sm text-white/75 font-medium">
+                1 sản phẩm của tdquang266 và hoang3402
+              </p>
+            </div>
+            <p className="text-sm text-white/75 font-medium">
+              © 2023 AnimeMoi. Tất cả quyền được bảo lưu.
+            </p>
+          </div>
+        </div>
       </div>
-    </>
+      <div className="tablet:hidden w-full h-full flex flex-col justify-center items-center gap-[50px]">
+        <div className="w-[400px] h-[200px] relative overflow-hidden">
+          <Image
+            src={Thumbnail}
+            alt=""
+            fill
+            sizes="(max-width: 390px) 100vw, (max-width: 1080px) 50vw"
+            className="object-contain"
+          />
+        </div>
+        <div className="w-[350px] flex flex-col gap-[8px] text-center">
+          <p className="text-[22px] text-lightGray font-semibold">
+            AnimeMoi mobile is coming
+          </p>
+          <p className="text-sm text-white/75 font-medium">
+            In the meantime, please use the desktop site for the best experience
+          </p>
+        </div>
+      </div>
+    </div>
   );
 }
