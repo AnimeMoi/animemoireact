5573d8314558440de47acc883d807913b27ae820
HoangDSR <nvh.02021995@gmail.com>
2023-09-29 14:03:18 +0700

    Implement Home button, logo button, fix image

--- app/pages/reader/[host]/page.tsx
+++ app/pages/reader/[host]/page.tsx
@@ -1,12 +1,16 @@
 import Image from "next/image";
 import Link from "next/link";
-import { Domain, DomainGetImage } from "../../../domain";
+import {Domain, DomainGetImage} from "../../../domain";
 
 export default async function Page(params: any) {
   const response = await fetch(
     `${Domain}${params.params.host}/ChapterDetail?url=${params.searchParams.id}`
   );
   const data = await response.json();
+  for (let i = 0; i < data.length; i++) {
+    if (data[i].includes("ntcdntemp"))
+      data[i] = `${DomainGetImage}${params.params.host}/GetImage?url=${data[i]}`
+  }
 
   return (
     <>
@@ -17,7 +21,7 @@ export default async function Page(params: any) {
         {data.map((chapter: any) => (
           <Image
             key={chapter}
-            src={`${DomainGetImage}${params.params.host}/GetImage?url=${chapter}`}
+            src={chapter}
             alt={""}
             width={"600"}
             height={"800"}
