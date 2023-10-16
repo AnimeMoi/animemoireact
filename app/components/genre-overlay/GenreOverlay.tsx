9b24dba68833fb40c0acabdd4f7a8e96e8159a70
HoangDSR <nvh.02021995@gmail.com>
2023-10-16 19:31:24 +0700

    Squashed commit of the following:
    commit b9a4fe892f6cfd126ee6b6cf524f97a4ec4df152
    Author: HoangDSR <nvh.02021995@gmail.com>
    Date:   Mon Oct 16 19:22:36 2023 +0700
    
        Update tags.json
    
    commit c81dd94220ab4dba266349ff27fa028997d9ac42
    Author: HoangDSR <nvh.02021995@gmail.com>
    Date:   Mon Oct 16 19:20:21 2023 +0700
    
        Fix genre

--- app/components/genre-overlay/GenreOverlay.tsx
+++ app/components/genre-overlay/GenreOverlay.tsx
@@ -1,5 +1,5 @@
 "use client";
-import React, { useEffect, useState } from "react";
+import React, { Dispatch, useEffect, useState } from "react";
 import "../../globals.css";
 import "./GenreOverlay.css";
 import NetTruyenGenres from "../../public/assets/genre-types/NetTruyen/tags.json";
@@ -12,57 +12,57 @@ import SayHentaiGenres from "../../public/assets/genre-types/SayHentai/tags.json
 import { useGlobalContext } from "../../context/store";
 
 type GenreOverlayProps = {
-  onGenreSelect: (genre: number) => void;
+	setSelectedGenre: Dispatch<React.SetStateAction<number | null>>;
 };
 
-const GenreOverlay: React.FC<GenreOverlayProps> = ({ onGenreSelect }) => {
-  const [genres, setGenres] = useState<any>([]);
-  const { selectedSource } = useGlobalContext();
+const GenreOverlay: React.FC<GenreOverlayProps> = ({ setSelectedGenre }) => {
+	const [genres, setGenres] = useState<any>([]);
+	const { selectedSource } = useGlobalContext();
 
-  useEffect(() => {
-    switch (selectedSource) {
-      case "NetTruyen":
-        setGenres(NetTruyenGenres);
-        break;
-      case "BaoTangTruyen":
-        setGenres(BaoTangTruyenGenres);
-        break;
-      case "CManga":
-        setGenres(CMangaGenres);
-        break;
-      case "Yurineko":
-        setGenres(YurinekoGenres);
-        break;
-      case "HentaiVN":
-        setGenres(HentaiVNGenres);
-        break;
-      case "LxManga":
-        setGenres(LxMangaGenres);
-        break;
-      case "SayHentai":
-        setGenres(SayHentaiGenres);
-        break;
-      default:
-        break;
-    }
-  }, [selectedSource]);
+	useEffect(() => {
+		switch (selectedSource) {
+			case "NetTruyen":
+				setGenres(NetTruyenGenres);
+				break;
+			case "BaoTangTruyen":
+				setGenres(BaoTangTruyenGenres);
+				break;
+			case "CManga":
+				setGenres(CMangaGenres);
+				break;
+			case "Yurineko":
+				setGenres(YurinekoGenres);
+				break;
+			case "HentaiVN":
+				setGenres(HentaiVNGenres);
+				break;
+			case "LxManga":
+				setGenres(LxMangaGenres);
+				break;
+			case "SayHentai":
+				setGenres(SayHentaiGenres);
+				break;
+			default:
+				break;
+		}
+	}, [selectedSource]);
 
-  return (
-    <div className="genre-wrapper bg-richBlack/60 backdrop-blur-[10px] rounded-3xl border-[1.5px] border-white/20 text-sm text-white/75 font-medium p-5">
-      <div className="h-[422px] grid gap-x-[45px] gap-y-5 grid-cols-5 overflow-y-auto no-scrollbar">
-        {genres.map((genre: any) => (
-          <div
-            key={genre.Id}
-            className="genre-name"
-            data-description={genre.Description}
-            onClick={() => onGenreSelect(genre.Id)}
-          >
-            {genre.Name}
-          </div>
-        ))}
-      </div>
-    </div>
-  );
+	return (
+		<div className="genre-wrapper bg-richBlack/60 backdrop-blur-[10px] rounded-3xl border-[1.5px] border-white/20 text-sm text-white/75 font-medium p-5">
+			<div className="h-[422px] grid gap-x-[45px] gap-y-5 grid-cols-5 overflow-y-auto no-scrollbar">
+				{genres.map((genre: any) => (
+					<div
+						key={genre.Id}
+						className="genre-name"
+						data-description={genre.Description}
+						onClick={() => setSelectedGenre(genre.Id)}
+					>
+						{genre.Name}
+					</div>
+				))}
+			</div>
+		</div>
+	);
 };
 
 export default GenreOverlay;
--- app/components/nav-bar/NavBar.tsx
+++ app/components/nav-bar/NavBar.tsx
@@ -17,210 +17,213 @@ import { useGlobalContext } from "../../context/store";
 import { NavBarProps, SearchParams } from "../../types/App";
 
 const NavBar: React.FC<NavBarProps> = ({ isHomePage }) => {
-  const [showOverlayType, setShowOverlayType] = useState<
-    "genre" | "signIn" | "signUp" | "accountSetting" | null
-  >(null);
-  const { setData, user, selectedSource } = useGlobalContext();
-  const [searchInput, setSearchInput] = useState("");
-  const [searchResults, setSearchResults] = useState([]);
-  const [isSearchResultVisible, setIsSearchResultVisible] = useState(false);
-  const [delayedChange, setDelayedChange] = useState("");
+	const [showOverlayType, setShowOverlayType] = useState<
+		"genre" | "signIn" | "signUp" | "accountSetting" | null
+	>(null);
+	const { setData, user, selectedSource } = useGlobalContext();
+	const [searchInput, setSearchInput] = useState("");
+	const [searchResults, setSearchResults] = useState([]);
+	const [isSearchResultVisible, setIsSearchResultVisible] = useState(false);
+	const [delayedChange, setDelayedChange] = useState("");
+	const [selectedGenre, setSelectedGenre] = useState<number | null>(null);
 
-  function handleAuthStateChanged(user: User) {
-    setShowOverlayType(null);
-  }
+	function handleAuthStateChanged(user: User) {
+		setShowOverlayType(null);
+	}
 
-  const handleOverlayToggle =
-    (type: "genre" | "signIn" | "signUp" | "accountSetting") => () => {
-      setShowOverlayType(type);
-    };
+	const handleOverlayToggle =
+		(type: "genre" | "signIn" | "signUp" | "accountSetting") => () => {
+			setShowOverlayType(type);
+		};
 
-  const showGenreOverlay = showOverlayType === "genre";
-  const showSignInOverlay = showOverlayType === "signIn";
-  const showSignUpOverlay = showOverlayType === "signUp";
-  const showAccountSettingOverlay = showOverlayType === "accountSetting";
+	const showGenreOverlay = showOverlayType === "genre";
+	const showSignInOverlay = showOverlayType === "signIn";
+	const showSignUpOverlay = showOverlayType === "signUp";
+	const showAccountSettingOverlay = showOverlayType === "accountSetting";
 
-  const handleOverlayClick = (e: React.MouseEvent) => {
-    if (e.target === e.currentTarget) {
-      setShowOverlayType(null);
-    }
-  };
+	const handleOverlayClick = (e: React.MouseEvent) => {
+		if (e.target === e.currentTarget) {
+			setShowOverlayType(null);
+		}
+	};
 
-  const handleButtonClick = () => (): void => {};
+	const handleButtonClick = () => (): void => {};
 
-  useEffect(() => {
-    const timeout = setTimeout(() => {
-      setDelayedChange(searchInput);
-    }, 300);
-    return () => clearTimeout(timeout);
-  }, [searchInput]);
+	useEffect(() => {
+		const timeout = setTimeout(() => {
+			setDelayedChange(searchInput);
+		}, 300);
+		return () => clearTimeout(timeout);
+	}, [searchInput]);
 
-  useEffect(() => {
-    if (delayedChange !== "") {
-      const fetchData = async (value: string) => {
-        const searchParams: SearchParams = {
-          query: value,
-          page: 0,
-          genres: [],
-          exclude: [],
-          status: 0,
-          host: selectedSource,
-        };
-        const data = await Search(searchParams);
-        if (data) {
-          setSearchResults(data);
-          setIsSearchResultVisible(data.length > 0);
-        }
-      };
+	useEffect(() => {
+		if (delayedChange !== "") {
+			const fetchData = async (value: string) => {
+				const searchParams: SearchParams = {
+					query: value,
+					page: 1,
+					genres: [],
+					exclude: [],
+					status: 0,
+					host: selectedSource,
+				};
+				const data = await Search(searchParams);
+				if (data) {
+					setSearchResults(data);
+					setIsSearchResultVisible(data.length > 0);
+				}
+			};
 
-      fetchData(delayedChange);
-    } else {
-      setIsSearchResultVisible(false);
-    }
-  }, [delayedChange, selectedSource]);
+			fetchData(delayedChange);
+		} else {
+			setIsSearchResultVisible(false);
+		}
+	}, [delayedChange, selectedSource]);
 
-  const clickToHideSearchResult = clickToHide(
-    "searchResult",
-    setIsSearchResultVisible
-  );
+	const clickToHideSearchResult = clickToHide(
+		"searchResult",
+		setIsSearchResultVisible
+	);
 
-  useEffect(() => {
-    document.addEventListener("click", clickToHideSearchResult);
+	useEffect(() => {
+		document.addEventListener("click", clickToHideSearchResult);
 
-    return () => {
-      document.removeEventListener("click", clickToHideSearchResult);
-    };
-  }, []);
+		return () => {
+			document.removeEventListener("click", clickToHideSearchResult);
+		};
+	}, []);
 
-  const [selectedGenre, setSelectedGenre] = useState<number | null>(null);
+	const GetComicByGenre = async () => {
+		if (!selectedGenre) return;
+		setData([]);
+		const searchParams: SearchParams = {
+			query: "",
+			page: 0,
+			genres: [selectedGenre],
+			exclude: [],
+			status: 0,
+			host: selectedSource,
+		};
+		const data = await Search(searchParams);
+		setData(data);
+	};
 
-  const handleGenreSelect = async (genre: number) => {
-    setData([]);
-    const searchParams: SearchParams = {
-      query: "",
-      page: 0,
-      genres: [genre],
-      exclude: [],
-      status: 0,
-      host: selectedSource,
-    };
-    const data = await Search(searchParams);
-    setData(data);
-    setSelectedGenre(genre);
-    setShowOverlayType(null);
-  };
+	useEffect(() => {
+		GetComicByGenre();
+		setShowOverlayType(null);
+	}, [selectedSource, selectedGenre]);
 
-  return (
-    <div className="w-full h-[90px] flex flex-row justify-between items-center bg-richBlack border-b-[1.5px] border-white/[.15]">
-      <div className="text-2xl text-lightGray font-semibold uppercase tracking-wider">
-        <Link href={`/`}>AnimeMoi</Link>
-      </div>
-      <div className="w-fit h-fit flex flex-row gap-[15px]">
-        {isHomePage ? null : (
-          <Link
-            href={`/`}
-            className="w-[48px] h-[48px] flex justify-center items-center rounded-full border-[1.5px] border-white/20"
-          >
-            <House color="#f4f4f4" weight="bold" size={18} />
-          </Link>
-        )}
-        <div className="relative">
-          <div className="w-[280px] h-[48px] flex flex-row items-center gap-2.5 px-[15px] rounded-full border-[1.5px] border-white/20">
-            <MagnifyingGlass color="#f4f4f4" weight="bold" size={18} />
-            <input
-              type="text"
-              placeholder="Tìm truyện"
-              value={searchInput}
-              onChange={(e) => setSearchInput(e.target.value)}
-              className="w-full h-full bg-transparent border-none outline-none placeholder:text-sm placeholder:text-white/75 placeholder:font-medium text-sm text-white/75 font-medium"
-            />
-          </div>
-          {isSearchResultVisible && (
-            <div
-              id="searchResult"
-              className="absolute top-[120%] left-0 z-[200]"
-            >
-              <SearchResult results={searchResults} />
-            </div>
-          )}
-        </div>
-        <div
-          className="w-fit h-[48px] flex flex-row items-center gap-2.5 px-[15px] rounded-full border-[1.5px] border-white/20 cursor-pointer"
-          onClick={handleOverlayToggle("genre")}
-        >
-          <List color="#f4f4f4" weight="bold" size={18} />
-          <p className="text-sm text-lightGray/75 font-medium">Thể loại</p>
-        </div>
-      </div>
-      {user ? (
-        <Image
-          src={Avatar}
-          alt={""}
-          className="scale-in w-[45px] h-[45px] rounded-full outline outline-[1.5px] outline-white/20 outline-offset-[-1.5px] cursor-pointer"
-          onClick={handleOverlayToggle("accountSetting")}
-        />
-      ) : (
-        <div className="w-fit h-fit flex flex-row gap-5 items-center">
-          <p
-            className="scale-in text-sm text-lightGray font-semibold cursor-pointer"
-            onClick={handleOverlayToggle("signIn")}
-          >
-            Đăng nhập
-          </p>
-          <div
-            className="scale-in w-fit h-fit px-[15px] py-[10px] bg-lightGray rounded-full cursor-pointer"
-            onClick={handleOverlayToggle("signUp")}
-          >
-            <p className="text-sm text-black font-semibold">Đăng ký</p>
-          </div>
-        </div>
-      )}
+	return (
+		<div className="w-full h-[90px] flex flex-row justify-between items-center bg-richBlack border-b-[1.5px] border-white/[.15]">
+			<div className="text-2xl text-lightGray font-semibold uppercase tracking-wider">
+				<Link href={`/`}>AnimeMoi</Link>
+			</div>
+			<div className="w-fit h-fit flex flex-row gap-[15px]">
+				{isHomePage ? null : (
+					<Link
+						href={`/`}
+						className="w-[48px] h-[48px] flex justify-center items-center rounded-full border-[1.5px] border-white/20"
+					>
+						<House color="#f4f4f4" weight="bold" size={18} />
+					</Link>
+				)}
+				<div className="relative">
+					<div className="w-[280px] h-[48px] flex flex-row items-center gap-2.5 px-[15px] rounded-full border-[1.5px] border-white/20">
+						<MagnifyingGlass color="#f4f4f4" weight="bold" size={18} />
+						<input
+							type="text"
+							placeholder="Tìm truyện"
+							value={searchInput}
+							onChange={(e) => setSearchInput(e.target.value)}
+							className="w-full h-full bg-transparent border-none outline-none placeholder:text-sm placeholder:text-white/75 placeholder:font-medium text-sm text-white/75 font-medium"
+						/>
+					</div>
+					{isSearchResultVisible && (
+						<div
+							id="searchResult"
+							className="absolute top-[120%] left-0 z-[200]"
+						>
+							<SearchResult results={searchResults} />
+						</div>
+					)}
+				</div>
+				<div
+					className="w-fit h-[48px] flex flex-row items-center gap-2.5 px-[15px] rounded-full border-[1.5px] border-white/20 cursor-pointer"
+					onClick={handleOverlayToggle("genre")}
+				>
+					<List color="#f4f4f4" weight="bold" size={18} />
+					<p className="text-sm text-lightGray/75 font-medium">Thể loại</p>
+				</div>
+			</div>
+			{user ? (
+				<Image
+					src={Avatar}
+					alt={""}
+					className="scale-in w-[45px] h-[45px] rounded-full outline outline-[1.5px] outline-white/20 outline-offset-[-1.5px] cursor-pointer"
+					onClick={handleOverlayToggle("accountSetting")}
+				/>
+			) : (
+				<div className="w-fit h-fit flex flex-row gap-5 items-center">
+					<p
+						className="scale-in text-sm text-lightGray font-semibold cursor-pointer"
+						onClick={handleOverlayToggle("signIn")}
+					>
+						Đăng nhập
+					</p>
+					<div
+						className="scale-in w-fit h-fit px-[15px] py-[10px] bg-lightGray rounded-full cursor-pointer"
+						onClick={handleOverlayToggle("signUp")}
+					>
+						<p className="text-sm text-black font-semibold">Đăng ký</p>
+					</div>
+				</div>
+			)}
 
-      {showGenreOverlay && (
-        <div
-          className="fixed inset-0 flex justify-center items-start pt-[90px] bg-richBlack/75 z-[200]"
-          onClick={handleOverlayClick}
-        >
-          <GenreOverlay onGenreSelect={handleGenreSelect} />
-        </div>
-      )}
+			{showGenreOverlay && (
+				<div
+					className="fixed inset-0 flex justify-center items-start pt-[90px] bg-richBlack/75 z-[200]"
+					onClick={handleOverlayClick}
+				>
+					<GenreOverlay setSelectedGenre={setSelectedGenre} />
+				</div>
+			)}
 
-      {showSignInOverlay && (
-        <div
-          className="fixed inset-0 flex justify-center items-center bg-richBlack/75 z-[200]"
-          onClick={handleOverlayClick}
-        >
-          <SignInOverlay onAuthStateChanged={handleAuthStateChanged} />
-        </div>
-      )}
+			{showSignInOverlay && (
+				<div
+					className="fixed inset-0 flex justify-center items-center bg-richBlack/75 z-[200]"
+					onClick={handleOverlayClick}
+				>
+					<SignInOverlay onAuthStateChanged={handleAuthStateChanged} />
+				</div>
+			)}
 
-      {showSignUpOverlay && (
-        <div
-          className="fixed inset-0 flex justify-center items-center bg-richBlack/75 z-[200]"
-          onClick={handleOverlayClick}
-        >
-          <SignUpOverlay onAuthStateChanged={handleAuthStateChanged} />
-        </div>
-      )}
+			{showSignUpOverlay && (
+				<div
+					className="fixed inset-0 flex justify-center items-center bg-richBlack/75 z-[200]"
+					onClick={handleOverlayClick}
+				>
+					<SignUpOverlay onAuthStateChanged={handleAuthStateChanged} />
+				</div>
+			)}
 
-      {/* Lớp phủ để làm nổi bật AccountSettingOverlay */}
-      {showAccountSettingOverlay && (
-        <div
-          className="fixed inset-0 bg-black/75 z-[150]"
-          onClick={handleOverlayClick}
-        />
-      )}
+			{/* Lớp phủ để làm nổi bật AccountSettingOverlay */}
+			{showAccountSettingOverlay && (
+				<div
+					className="fixed inset-0 bg-black/75 z-[150]"
+					onClick={handleOverlayClick}
+				/>
+			)}
 
-      {showAccountSettingOverlay && (
-        <div className="absolute top-[90%] right-0 z-[200]">
-          <AccountSettingOverlay
-            onEdit={handleButtonClick}
-            onClose={() => setShowOverlayType(null)}
-          />
-        </div>
-      )}
-    </div>
-  );
+			{showAccountSettingOverlay && (
+				<div className="absolute top-[90%] right-0 z-[200]">
+					<AccountSettingOverlay
+						onEdit={handleButtonClick}
+						onClose={() => setShowOverlayType(null)}
+					/>
+				</div>
+			)}
+		</div>
+	);
 };
 
 export default NavBar;
